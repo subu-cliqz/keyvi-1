e26806dec56c9528eba8661d43453ce2772803af
Travis CI <travis@travis-ci.org>
2018-08-28 09:36:09 +0000

    Deploy code docs to GitHub Pages Travis build: 405
    Commit: de757ef7ba7eb3e3c362f5f5c174a616ddd6061b

--- html/navtreeindex0.js
+++ html/navtreeindex0.js
@@ -1,14 +1,5 @@
 var NAVTREEINDEX0 =
 {
 "index.html":[],
-"md_doc_algorithm__construction-_basics.html":[0],
-"md_doc_algorithm__extensibility.html":[1],
-"md_doc_algorithm__minimization.html":[2],
-"md_doc_algorithm__persistence-_basics.html":[3],
-"md_doc_algorithm__scaling.html":[4],
-"md_doc_usage__building_keyvi_dictionaries.html":[6],
-"md_doc_usage__building_keyvi_dictionaries_with_python.html":[5],
-"md_doc_usage__crashcourse.html":[7],
-"md_doc_usage__using_pykeyvi_in__e_m_r.html":[8],
 "pages.html":[]
 };
--- html/search/all_6.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['the_20python_20keyvi_20compilers',['The python keyvi compilers',['../md_doc_usage__building_keyvi_dictionaries_with_python.html',1,'']]]
-];
